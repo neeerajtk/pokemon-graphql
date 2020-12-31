010a0dcfd3c2cc4f61b1433cd7dfdacf7391f398
neeerajtk <neerajtk560@gmail.com>
2020-12-31 23:34:27 +0530

    base graphql req added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import React from "react";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "@apollo/react-hooks";
 
 function App() {
+  const client = new ApolloClient({
+    uri: "https://graphql-pokemon2.vercel.app",
+  });
   return (
-    <>
-      <h3>Graph ql</h3>
-    </>
+    <ApolloProvider client={client}>
+      <main>
+        <p>pokemon</p>
+      </main>
+    </ApolloProvider>
   );
 }
 
