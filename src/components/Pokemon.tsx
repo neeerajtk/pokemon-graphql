516c1a57fabdc06d8a29b4e33bf7fa2d47f0d775
neeerajtk <neerajtk560@gmail.com>
2021-01-02 23:51:58 +0530

    display component added

---
+++ src/components/Pokemon.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Pokemon(pokemon: any) {
+  return <div>hello</div>;
+}
--- src/containers/Pokemons.tsx
+++ src/containers/Pokemons.tsx
@@ -1,15 +1,18 @@
 import React from "react";
 import { useQuery } from "@apollo/react-hooks";
 import { GET_POKEMONS } from "../graphql/getPokemons";
+import Pokemon from "../components/Pokemon";
 
 export function Pokemons() {
   const { data: { pokemons = [] } = {} } = useQuery(GET_POKEMONS, {
-    variables: { first: 9 },
+    variables: { first: 5 },
   });
   return (
     <div className="pokemons">
       {pokemons.map((pokemon: any) => (
-        <p>{JSON.stringify(pokemon)}</p>
+        <p>
+          <Pokemon pokemon={pokemon} />
+        </p>
       ))}
     </div>
   );
